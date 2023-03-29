3a521aa1c990dd84182c5eb243c3fadcfaa48211
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-03-29 12:19:46 +0200

    fix(projects/list): query params validation

--- pkgs/api/src/routes/v0/projects/list.ts
+++ pkgs/api/src/routes/v0/projects/list.ts
@@ -1,6 +1,10 @@
 import type { FastifyPluginCallback } from 'fastify';
+import { z } from 'zod';
 
+import { validationError } from '../../../common/errors';
 import { toApiProject } from '../../../common/formatters/project';
+import { valOrgId } from '../../../common/zod';
+import type { Perm } from '../../../models';
 import { Project } from '../../../models';
 import type {
   Pagination,
@@ -8,10 +12,25 @@ import type {
   ResListProjects,
 } from '../../../types/api';
 
+function QueryVal(perms: Perm[]) {
+  const q: Record<keyof ReqListProjects, any> = {
+    org_id: valOrgId(perms),
+  };
+  return z.object(q).strict();
+}
+
 const fn: FastifyPluginCallback = async (fastify, _, done) => {
   fastify.get<{ Querystring: ReqListProjects; Reply: ResListProjects }>(
     '/',
     async function (req, res) {
+      const val = QueryVal(req.perms!).safeParse(req.query);
+      if (!val.success) {
+        validationError(res, val.error);
+        return;
+      }
+
+      const query = val.data;
+
       // TODO: pagination
       const pagination: Pagination = {
         currentPage: 0,
@@ -20,11 +39,10 @@ const fn: FastifyPluginCallback = async (fastify, _, done) => {
 
       const projects = await Project.findAll({
         where: {
-          // TODO: validation
-          orgId: req.query.org_id,
+          orgId: query.org_id,
         },
         order: [['name', 'ASC']],
-        limit: 10,
+        limit: 20,
         offset: 0,
       });
 
