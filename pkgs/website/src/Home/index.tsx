8b16030869326c97e21408590ea126bbc8fffbe9
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-03-29 12:03:50 +0200

    fix(website): comment gradient

--- pkgs/website/src/Home/index.tsx
+++ pkgs/website/src/Home/index.tsx
@@ -9,15 +9,23 @@ export const Home: React.FC = () => {
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-left">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Specfy
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Next-gen documentation platform for engineers.
-            </p>
-            <p className="text-lg leading-8 text-gray-600">Coming soon.</p>
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+        >
+          {/* <div className="blur-[106px]  h-64 bg-gradient-to-r from-indigo-400 to-indigo-100"></div> */}
+        </div>
+        <div className="relative">
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="text-left">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Specfy
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-800">
+                Next-gen documentation platform for engineers.
+              </p>
+              <p className="text-lg leading-8 text-gray-700">Coming soon.</p>
+            </div>
           </div>
         </div>
       </div>
--- pkgs/website/tailwind.config.js
+++ pkgs/website/tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // colors: {
+    //   ...colors,
+    // },
+    extend: {
+      colors: {
+        // primary: colors.indigo,
+        // secondary: colors.yellow,
+        // neutral: colors.gray,
+      },
+    },
   },
   plugins: [],
 };
