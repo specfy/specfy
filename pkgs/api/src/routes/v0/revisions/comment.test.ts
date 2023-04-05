b8007c5e4676bc268baed60bff01824158543a9a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-04-05 15:17:09 +0200

    tests: POST /revisions/:id/comment

---
+++ pkgs/api/src/routes/v0/revisions/comment.test.ts
@@ -0,0 +1,63 @@
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
+
+import type { TestSetup } from '../../../test/each';
+import { setupBeforeAll, setupAfterAll } from '../../../test/each';
+import { isSuccess, isValidationError } from '../../../test/fetch';
+import {
+  shouldBeProtected,
+  shouldNotAllowQueryParams,
+} from '../../../test/helpers';
+import { seedRevision } from '../../../test/seed/revisions';
+import { seedSimpleUser, seedWithProject } from '../../../test/seed/seed';
+
+let t: TestSetup;
+beforeAll(async () => {
+  t = await setupBeforeAll();
+});
+
+afterAll(async () => {
+  await setupAfterAll(t);
+});
+
+describe('GET /revisions/:revision_id/comment', () => {
+  it('should be protected', async () => {
+    const res = await t.fetch.post('/0/revisions/foo/comment');
+    await shouldBeProtected(res);
+  });
+
+  it('should not allow query params', async () => {
+    const { token } = await seedSimpleUser();
+    const res = await t.fetch.post('/0/revisions/foo/comment', {
+      token,
+      // @ts-expect-error
+      qp: { random: 'world' },
+    });
+    await shouldNotAllowQueryParams(res);
+  });
+
+  it('should reject invalid body', async () => {
+    const { token, org, project, user } = await seedWithProject();
+    const revision = await seedRevision(user, org, project);
+
+    const res = await t.fetch.post(`/0/revisions/${revision.id}/comment`, {
+      token,
+      qp: { org_id: org.id, project_id: project.id },
+      // @ts-expect-error
+      body: {},
+    });
+    isValidationError(res.json);
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should post a comment', async () => {
+    const { token, org, project, user } = await seedWithProject();
+    const revision = await seedRevision(user, org, project);
+
+    const comment = await t.fetch.post(`/0/revisions/${revision.id}/comment`, {
+      token,
+      qp: { org_id: org.id, project_id: project.id },
+      body: { approval: false, content: { content: [], type: 'doc' } },
+    });
+    isSuccess(comment);
+  });
+});
