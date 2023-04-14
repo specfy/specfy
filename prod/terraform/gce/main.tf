b913bec9bd9a811f2c3b8fe3c542837327afbedc
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-04-14 19:18:36 +0200

    fix(tf): almost there ?

--- prod/terraform/gce/main.tf
+++ prod/terraform/gce/main.tf
@@ -3,45 +3,32 @@ resource "google_service_account" "default" {
   display_name = "GCE Main"
 }
 
-locals {
-  spec = {
-    spec = {
-      containers = [{
-        name = "main"
-        image   = "gcr.io/specfy/main"
-        command = ["node"]
-        args    = ["pkgs/api/build/index.js"]
-        stdin = false
-        tty = false
-      }]
-      restartPolicy = true
-    }
-  }
-
-  spec_as_yaml = yamlencode(local.spec)
-}
-
 resource "google_compute_instance" "main" {
   name         = "main"
-  machine_type = "e2-micro"
+  machine_type = "e2-small"
   zone         = "${var.envs.region}-a"
 
-  can_ip_forward      = false
+  can_ip_forward = false
   # deletion_protection = true
-  enable_display      = false
+  enable_display            = false
+  allow_stopping_for_update = true
 
   labels = {
-    "container-vm" = "main"
+    "container-vm" = "cos-101-17162-127-57"
+    "ec-src"       = "vm_add-tf"
   }
 
   boot_disk {
+    mode = "READ_WRITE"
+
     initialize_params {
-      image = "https://www.googleapis.com/compute/v1/projects/cos-cloud/global/images/cos-stable-105-17412-1-61"
+      image = "https://www.googleapis.com/compute/v1/projects/cos-cloud/global/images/cos-101-17162-127-57"
+      size  = 10
+      type  = "pd-balanced"
     }
   }
 
   network_interface {
-
     access_config {
       network_tier = "PREMIUM"
     }
@@ -49,17 +36,41 @@ resource "google_compute_instance" "main" {
     subnetwork = var.network.subId
   }
 
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
   metadata = {
-    gce-container-declaration = local.spec_as_yaml
+    gce-container-declaration = <<-EOT
+      spec:
+        containers:
+        - name: main
+          image: gcr.io/specfy/main
+          command:
+          - node
+          args:
+          - pkgs/api/build/index.js
+          stdin: false
+          tty: false
+        restartPolicy: Always
+      # This container declaration format is not public API and may change without notice. Please
+      # use gcloud command-line tool or Google Cloud Console to run Containers on Google Compute Engine.
+    EOT
   }
 
   service_account {
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform",  "devstorage.read_only",
-               "logging.write",
-               "monitoring.write",
-               "service.management.readonly",
-               "servicecontrol",
-               "trace.append"]
+    email = google_service_account.default.email
+    scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
   }
 }
