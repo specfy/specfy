8a786a1da8f7ecab36bfb6f0dcc93dd27e85c9ad
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-03 11:05:09 +0200

    tests: add missing Github tests

---
+++ pkgs/api/src/routes/v0/github/installations.test.ts
@@ -0,0 +1,35 @@
+import { beforeAll, afterAll, describe, it } from 'vitest';
+
+import type { TestSetup } from '../../../test/each.js';
+import { setupAfterAll, setupBeforeAll } from '../../../test/each.js';
+import {
+  shouldBeProtected,
+  shouldNotAllowQueryParams,
+} from '../../../test/helpers.js';
+import { seedSimpleUser } from '../../../test/seed/seed.js';
+
+let t: TestSetup;
+beforeAll(async () => {
+  t = await setupBeforeAll();
+});
+
+afterAll(async () => {
+  await setupAfterAll(t);
+});
+
+describe('POST /github/installations', () => {
+  it('should be protected', async () => {
+    const res = await t.fetch.get('/0/github/installations');
+    await shouldBeProtected(res);
+  });
+
+  it('should not allow query params', async () => {
+    const { token } = await seedSimpleUser();
+    const res = await t.fetch.get('/0/github/installations', {
+      token,
+      // @ts-expect-error
+      Querystring: { random: 'world' },
+    });
+    await shouldNotAllowQueryParams(res);
+  });
+});
---
+++ pkgs/api/src/routes/v0/github/webhooks.test.ts
@@ -0,0 +1,32 @@
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
+
+import type { TestSetup } from '../../../test/each.js';
+import { setupAfterAll, setupBeforeAll } from '../../../test/each.js';
+import { shouldNotAllowQueryParams } from '../../../test/helpers.js';
+import { seedSimpleUser } from '../../../test/seed/seed.js';
+
+let t: TestSetup;
+beforeAll(async () => {
+  t = await setupBeforeAll();
+});
+
+afterAll(async () => {
+  await setupAfterAll(t);
+});
+
+describe('POST /github/webhooks', () => {
+  it('should not be protected ny regular auth', async () => {
+    const res = await t.fetch.post('/0/github/webhooks');
+    expect(res.statusCode).toBe(403);
+  });
+
+  it('should not allow query params', async () => {
+    const { token } = await seedSimpleUser();
+    const res = await t.fetch.get('/0/github/repos', {
+      token,
+      // @ts-expect-error
+      Querystring: { random: 'world' },
+    });
+    await shouldNotAllowQueryParams(res);
+  });
+});
