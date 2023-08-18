8f04a53b327a9b44cc1fd55a436ff967815c6a50
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-18 22:35:44 +0200

    jobs: renames jobs to service

---
+++ pkgs/github/src/service.ts
@@ -0,0 +1,60 @@
+import { l as logger, metrics } from '@specfy/core';
+import { prisma } from '@specfy/db';
+
+import type { Job } from './Job.js';
+import { JobDeploy } from './jobs/deploy.js';
+
+let interval: NodeJS.Timeout;
+const running: Job[] = [];
+const l = logger.child({ svc: 'jobs' });
+
+export async function off() {
+  l.info('Exiting');
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  await Promise.all(running);
+}
+
+export function listen() {
+  l.info('Starting');
+
+  // TODO: replace this with a queue and/or Listen/notify
+  interval = setInterval(async () => {
+    metrics.increment('jobs.loop');
+    await prisma.$transaction(async (tx) => {
+      const next = await tx.jobs.findFirst({
+        where: {
+          status: 'pending',
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+        include: {
+          Org: true,
+          Project: true,
+          User: true,
+        },
+      });
+      if (!next) {
+        return;
+      }
+
+      await tx.jobs.update({
+        data: {
+          status: 'running',
+          startedAt: new Date(),
+          updatedAt: new Date(),
+        },
+        where: {
+          id: next.id,
+        },
+      });
+
+      const job = new JobDeploy(next);
+      running.push(job);
+      (() => job.start())();
+    });
+  }, 5000);
+}
