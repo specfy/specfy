2a461aecb045363a5bda0640b996acdbfe829ea6
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-24 17:47:34 +0200

    lint

--- pkgs/core/src/metric.ts
+++ pkgs/core/src/metric.ts
@@ -3,7 +3,7 @@ import ddMetrics from 'datadog-metrics';
 import { envs } from './env.js';
 
 ddMetrics.init({
-  apiKey: envs.DD_API_KEY,
+  apiKey: envs.DD_API_KEY || '',
   prefix: 'specfy.',
   defaultTags: [`env:${process.env.NODE_ENV}`],
   reporter:
