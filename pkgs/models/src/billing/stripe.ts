1b6e65cf2120dc83b5ca5d6cff7386ae0f74b7af
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-24 17:53:11 +0200

    lint

--- pkgs/models/src/billing/stripe.ts
+++ pkgs/models/src/billing/stripe.ts
@@ -1,7 +1,7 @@
 import { envs } from '@specfy/core';
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(envs.STRIPE_KEY, {
+export const stripe = new Stripe(envs.STRIPE_KEY || '', {
   apiVersion: '2022-11-15',
   typescript: true,
   telemetry: false,
