9710322382c203b6facc1e85916d6ad12fbd3613
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-31 17:35:48 +0200

    Update index.tsx
    Signed-off-by: Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>

--- pkgs/app/src/components/Github/RepoSelect/index.tsx
+++ pkgs/app/src/components/Github/RepoSelect/index.tsx
@@ -2,8 +2,6 @@ import { IconPlus } from '@tabler/icons-react';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
-import { GITHUB_APP } from '../../../common/envs';
-import { Popup } from '../../../common/popup';
 import { useGetGitHubRepos } from '../../../api';
 import { useToast } from '../../../hooks/useToast';
 import { CommandItem } from '../../Command';
