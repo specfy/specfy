1029dec6ec8afe620ffee160834b2e226486660e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-11-13 12:37:31 +0100

    f

--- pkgs/app/src/common/diff/prosemirror.test.ts
+++ pkgs/app/src/common/diff/prosemirror.test.ts
@@ -827,6 +827,8 @@ describe('diff codeBlock', () => {
             hunks: [
               {
                 content: '@@ -1,3 +1,3 @@',
+                // @ts-expect-error
+                isPlain: false,
                 newLines: 3,
                 newStart: 1,
                 oldLines: 3,
