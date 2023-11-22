4f26c5a61632fd4b7f360385f88a74d7e732fa0d
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-11-22 17:31:22 +0100

    fix: dependencies 2023-11-19 (#348)
    Weekly dependencies update.
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- prod/terraform/main.tf
+++ prod/terraform/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.78.0"
+      version = "4.84.0"
     }
     vercel = {
       source  = "vercel/vercel"
-      version = "~> 0.15"
+      version = "~> 0.16"
     }
   }
 }
